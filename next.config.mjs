07bc83880cceaef9c0b28d9172e4ca683891dcd0
BlaiseMcNamara <blaise@outlook.com.au>
2025-09-16 21:02:59 +1000

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true };
-export default nextConfig;
+const nextConfig = {
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value:
+              "frame-ancestors 'self' https://*.myshopify.com https://*.shopify.com https://admin.shopify.com https://*.shop.app https://offgriddoc.com.au https://www.offgriddoc.com.au"
+          },
+          { key: 'Referrer-Policy', value: 'no-referrer-when-downgrade' }
+        ]
+      }
+    ]
+  }
+}
+export default nextConfig
